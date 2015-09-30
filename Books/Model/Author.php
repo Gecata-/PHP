6aed55f7b15d0a77993ebd1e95480197ba1b1f90
Georgi Dimitrov <gdimitrov@GDimitrov.IBSBG.BG>
2015-09-30 13:13:10 +0300

    Revert "Revert "Testing MVC with LibraryModel""
    This reverts commit 2977abe1b7b81c1ea65e1e57de83da3eea3f90f0.

---
+++ Books/Model/Author.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: gdimitrov
+ * Date: 30.9.2015 ã.
+ * Time: 12:17 ÷.
+ */
+include '../DB/mySQL.php';
+
+class Author
+{
+
+    private $host = 'localhost';
+    private $user = 'Gecata';
+    private $password = '1234';
+    private $database = 'books';
+    private $connection;
+
+    public function __construct()
+    {
+        $mysql = mySQL::getInstance();
+        $mysql->connect($this->host, $this->user, $this->password, $this->database);
+        $this->connection = $mysql->getConnection();
+    }
+
+    public function insertAuthor($name)
+    {
+        $stmt = mysqli_prepare($this->connection, 'INSERT INTO authors(author_name) VALUES (?)');
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, 's', $name);
+            mysqli_stmt_execute($stmt);
+        } else {
+            throw new Exception('Couldn`t insert author');
+        }
+    }
+
+    public function checkRepeatName($name)
+    {
+        $stmt = mysqli_prepare($this->connection, 'SELECT author_name FROM authors WHERE author_name =?');
+        mysqli_stmt_bind_param($stmt, 's', $name);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $hasAuthor);
+        mysqli_stmt_fetch($stmt);
+        return $hasAuthor;
+    }
+
+    public function selectAllAuthors()
+    {
+        $authors =[];
+        $stmt = mysqli_prepare($this->connection, 'SELECT author_name FROM authors');
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $author);
+        while(mysqli_stmt_fetch($stmt)){
+            $authors[] = $author;
+        };
+        return $authors;
+    }
+
+}
---
+++ Books/View/books.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gdimitrov
+ * Date: 30.9.2015 г.
+ * Time: 10:06 ч.
+ */
+$title = ' ниги от автор';
+include 'head.php';
+?>
+
+
