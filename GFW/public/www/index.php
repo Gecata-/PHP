acca950b3d9f3ad877ff2f9122e48d3392c91305
Georgi Dimitrov <gb_dimitrov@yahoo.bg>
2015-10-02 00:08:47 +0300

    First steps of my own PHP Framework

---
+++ GFW/public/www/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gdimitrov
+ * Date: 1.10.2015 ã.
+ * Time: 13:02 ÷.
+ */
+
+include '../../GecataFrameWork/App.php';
+$app = GF\App::getInstance();
+$app->run();
+
+
+new \GF\PLQS();
